3edc62829364c5480faae194dd9e2e2f8df99b17
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-04 09:19:00 +0330

    fea: close the typeorm connection when the server stops/restarts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -10,3 +10,21 @@ export const AppDataSource = new DataSource({
   entities: [],
   synchronize: process.env.NODE_ENV !== 'production',
 });
+
+process.on('exit', async () => {
+  console.log('Closing TypeORM connection...');
+  await AppDataSource.destroy();
+});
+
+process.on('SIGINT', async () => {
+  console.log('Closing TypeORM connection...');
+  await AppDataSource.destroy();
+  process.exit(0);
+});
+
+process.on('unhandledRejection', async (err) => {
+  console.error('Unhandled rejection:', err);
+  console.log('Closing TypeORM connection...');
+  await AppDataSource.destroy();
+  process.exit(1);
+});
