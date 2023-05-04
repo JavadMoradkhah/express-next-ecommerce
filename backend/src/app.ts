3c6faec629e463bc3cad2d83a2abb28ce57a3741
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-04 09:37:38 +0330

    feat: connect to the database on server bootstrapping

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import helmet from 'helmet';
+import { AppDataSource } from './config/database';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) ?? 5000;
@@ -13,6 +14,13 @@ app.get('/api/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running: http://localhost:${port}/api/`);
+app.listen(port, async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log('✅ Database connection has been established successfully');
+    console.log(`🟢 Server is running: http://localhost:${port}/api/`);
+  } catch (error) {
+    console.log(error);
+  }
+});
 });
