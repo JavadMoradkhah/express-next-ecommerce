f5f4efb1b7f3518d46c1c9f4833ca4083a7ae975
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-04 12:57:03 +0330

    feat: replace the exception middleware with route handler middleware

---
+++ backend/src/middleware/route-handler.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCode } from '../enums/status-code.enum';
+import { StatusCodeName } from '../enums/status-code-name.enum';
+
+type RequestHandler = (req: Request, res: Response) => Promise<any>;
+
+export default function (handler: RequestHandler, statusCode = StatusCode.OK) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await handler(req, res);
+
+      const status =
+        statusCode !== StatusCode.OK
+          ? statusCode
+          : req.method.toLowerCase() === 'post'
+          ? StatusCode.CREATED
+          : StatusCode.OK;
+
+      res.status(status).send({
+        statusCode: status,
+        data: response,
+        ...(status !== StatusCode.OK &&
+          status !== StatusCode.CREATED && { message: StatusCodeName?.[status] }),
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
