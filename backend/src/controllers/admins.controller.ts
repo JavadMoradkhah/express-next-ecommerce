3033d3a92453aff86c5a057006ade31070573dab
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-05 19:51:09 +0330

    feat: build the admin controller with "findOne" action

---
+++ backend/src/controllers/admins.controller.ts
@@ -0,0 +1,21 @@
+import { adminsRepo } from '../repositories';
+import { BadRequestException } from '../common/exceptions';
+import ErrorMessages from '../enums/error-messages.enum';
+
+export const findOne = async (id: string) => {
+  const admin = await adminsRepo.findOne({
+    where: { id },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      role: true,
+    },
+  });
+
+  if (!admin) {
+    throw new BadRequestException(ErrorMessages.INVALID_USER_CREDENTIALS);
+  }
+
+  return admin;
+};
