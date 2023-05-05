af6830d3554257d39ec3924b3375dcb1e1aa02ab
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-05 19:59:15 +0330

    feat: build the admin local strategy

---
+++ backend/src/auth/strategies/admin-local.strategy.ts
@@ -0,0 +1,15 @@
+import { Strategy as LocalStrategy } from 'passport-local';
+import { validateAdminCredentials } from '../../controllers/auth.controller';
+import { UnauthorizedException } from '../../common/exceptions';
+import { SessionAdminUser } from '../../interfaces';
+import ErrorMessages from '../../enums/error-messages.enum';
+
+export default new LocalStrategy(async (username: string, password: string, done) => {
+  const admin: SessionAdminUser = await validateAdminCredentials(username, password);
+
+  if (!admin) {
+    return done(new UnauthorizedException(ErrorMessages.INVALID_USERNAME_OR_PASSWORD));
+  }
+
+  done(null, admin);
+});
