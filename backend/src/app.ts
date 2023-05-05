daa8704d2cc1f9b55d1a2b7e1508a6cfc80ea0f4
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-05 23:21:36 +0330

    feat: set an expiration time for session Redis store and session cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,7 +35,20 @@ app.use(
     saveUninitialized: false,
     store: new RedisStore({
       client: redisClient,
+      ttl: 15 * 60, // 15 minutes
+      disableTouch: true,
+      /* 
+        ** Disables resetting the TTL when using touch **
+        The express-session package uses touch
+        to signal to the store that the user has interacted
+        with the session but hasn't changed anything in its data.
+        Typically, this helps keep the users session alive
+        if session changes are infrequent but you may want to disable it
+        to cut down the extra calls or to prevent users from keeping sessions open too long.
+        Also consider enabling if you store a lot of data on the session.
+      */
     }),
+    cookie: { maxAge: 15 * 60 * 1000 /* 15 minutes */ },
   })
 );
 app.use(cookieParser());
