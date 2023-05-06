5d5adccdfb2c817d662de5a038dcc3d81ab1b2b7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-06 09:42:28 +0330

    feat: export the size repo from directory index module

--- backend/src/repositories/index.ts
+++ backend/src/repositories/index.ts
@@ -3,4 +3,5 @@ export * from './admins.repo';
 export * from './colors.repo';
 export * from './countries.repo';
 export * from './shipping-methods.repo';
+export * from './sizes.repo';
 export * from './uploads.repo';
