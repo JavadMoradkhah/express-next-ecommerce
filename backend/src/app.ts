56d5f6c4e81f9493a5bb5711ce5765147cc85f0f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-06 20:11:18 +0330

    feat: add the express urlencoded middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,6 +33,7 @@ if (app.get('env') === 'development') {
 
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
