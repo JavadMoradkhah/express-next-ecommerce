16b5bc635b529ee9aa9dd730520e17a23e0e41d3
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-06 08:46:21 +0330

    feat: build the countries controller

---
+++ backend/src/controllers/countries.controller.ts
@@ -0,0 +1,59 @@
+import { ConflictException, NotFoundException } from '../common/exceptions';
+import { CreateCountryDto, UpdateCountryDto } from '../dto';
+import { countriesRepo } from '../repositories';
+import ErrorMessages from '../enums/error-messages.enum';
+
+export const findAll = async () => {
+  const countries = await countriesRepo.find({
+    order: {
+      name: 'ASC',
+    },
+  });
+
+  return countries;
+};
+
+export const findOne = async (id: string) => {
+  const country = await countriesRepo.findOneBy({ id });
+
+  if (!country) {
+    throw new NotFoundException(ErrorMessages.COUNTRY_NOT_FOUND);
+  }
+
+  return country;
+};
+
+export const create = async ({ name }: CreateCountryDto) => {
+  await checkConflicts(name);
+
+  const country = countriesRepo.create({
+    name: name,
+  });
+
+  return await countriesRepo.save(country);
+};
+
+export const update = async (id: string, { name }: UpdateCountryDto) => {
+  const country = await findOne(id);
+
+  await checkConflicts(name);
+
+  country.name = name;
+
+  return await countriesRepo.save(country);
+};
+
+export const remove = async (id: string) => {
+  const country = await findOne(id);
+  await countriesRepo.remove(country);
+};
+
+async function checkConflicts(name: string): Promise<void | never> {
+  const exists = await countriesRepo.exist({
+    where: { name },
+  });
+
+  if (exists) {
+    throw new ConflictException(ErrorMessages.COUNTRY_ALREADY_EXISTS);
+  }
+}
