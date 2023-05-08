3e9b2911006a0ba194ff9f191083701aa1424c0d
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-08 18:45:59 +0330

    feat: build the product images controller

---
+++ backend/src/controllers/product-images.controller.ts
@@ -0,0 +1,63 @@
+import { NotFoundException } from '../common/exceptions';
+import { CreateProductImageDto, UpdateProductImageDto } from '../dto';
+import { productImagesRepo } from '../repositories';
+import * as productsController from './products.controller';
+import * as uploadsController from './uploads.controller';
+import ErrorMessages from '../enums/error-messages.enum';
+
+export const findOne = async (id: string) => {
+  const productImage = await productImagesRepo.findOneBy({ id });
+
+  if (!productImage) {
+    throw new NotFoundException(ErrorMessages.PRODUCT_IMAGE_NOT_FOUND);
+  }
+
+  return productImage;
+};
+
+export const create = async (createImageDto: CreateProductImageDto) => {
+  const product = await productsController.findOne(createImageDto.product);
+
+  const image = await uploadsController.findOne(createImageDto.image);
+
+  // Check whether the uploaded image is marked as the product main image or not
+  if (createImageDto.isMain) {
+    // Find the product's main image to unmark it as the main image
+    const mainProductImage = await productImagesRepo.findOneBy({
+      product: {
+        id: product.id,
+      },
+      isMain: true,
+    });
+
+    if (mainProductImage) {
+      mainProductImage.isMain = false;
+      await productImagesRepo.save(mainProductImage);
+    }
+  }
+
+  const productImage = productImagesRepo.create({
+    product: {
+      id: product.id,
+    },
+    image: {
+      id: image.id,
+    },
+    isMain: createImageDto.isMain,
+  });
+
+  return await productImagesRepo.save(productImage);
+};
+
+export const update = async (id: string, { isMain }: UpdateProductImageDto) => {
+  const image = await findOne(id);
+
+  image.isMain = isMain;
+
+  return await productImagesRepo.save(image);
+};
+
+export const remove = async (id: string) => {
+  const productImage = await findOne(id);
+  await productImagesRepo.remove(productImage);
+};
