9dff204f646eb05367cb1fbdb88a4c1b329ddc3d
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-10 13:46:22 +0330

    feat: add users registration functionality

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,11 @@
+import { Response } from 'express';
 import * as bcrypt from 'bcrypt';
 import { Request } from 'express';
-import { adminsRepo } from '../repositories';
+import { adminsRepo, usersRepo } from '../repositories';
 import { SessionAdminUser } from '../interfaces';
+import { CreateUserDto } from '../dto';
+import { ConflictException } from '../common/exceptions';
+import ErrorMessages from '../enums/error-messages.enum';
 
 export const validateAdminCredentials = async (
   username: string,
@@ -22,4 +26,23 @@ export const validateAdminCredentials = async (
 
 export const loginAdmin = async (req: Request) => {
   return req.user;
+};
+
+export const registerUser = async (createUserDto: CreateUserDto, res: Response) => {
+  const userExists = await usersRepo.exist({ where: { email: createUserDto.email } });
+
+  if (userExists) {
+    throw new ConflictException(ErrorMessages.EMAIL_CONFLICT);
+  }
+
+  const user = usersRepo.create({
+    firstName: createUserDto.firstName,
+    lastName: createUserDto.lastName,
+    email: createUserDto.email,
+    password: createUserDto.password,
+  });
+
+  await usersRepo.save(user);
+
+  return null;
 };
--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,17 +1,19 @@
 import { Router, Request, Response } from 'express';
-import routeHandler from '../middleware/route-handler';
-import { StatusCode } from '../enums/status-code.enum';
-import schemaValidator from '../middleware/schema-validator';
-import { loginSchema } from '../schemas/admin.schema';
 import passport from 'passport';
-import { loginAdmin } from '../controllers/auth.controller';
+import routeHandler from '../middleware/route-handler';
+import schemaValidator from '../middleware/schema-validator';
+import { loginSchema as adminLoginSchema } from '../schemas/admin.schema';
+import { createSchema } from '../schemas/user.schema';
+import { StatusCode } from '../enums/status-code.enum';
+import { CreateUserDto } from '../dto';
+import { loginAdmin, registerUser } from '../controllers/auth.controller';
 import adminAuth from '../middleware/admin-auth';
 
 const router = Router();
 
 router.post(
   '/admin/login',
-  schemaValidator(loginSchema),
+  schemaValidator(adminLoginSchema),
   passport.authenticate('ADMIN_LOCAL_STRATEGY'),
   routeHandler((req: Request, res: Response) => {
     return loginAdmin(req);
@@ -24,6 +26,14 @@ router.get(
   routeHandler((req: Request, res: Response) => {
     return req.user;
   })
+);
+
+router.post(
+  '/signup',
+  schemaValidator(createSchema),
+  routeHandler((req: Request, res: Response) => {
+    return registerUser(req.body as CreateUserDto, res);
+  }, StatusCode.OK)
 );
 
 export default router;
