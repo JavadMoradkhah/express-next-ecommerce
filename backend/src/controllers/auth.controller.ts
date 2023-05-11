2c63347856b342cbe5f75dafcc4f03025bd81cc2
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-11 18:49:07 +0330

    feat: implement email verification feature

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Response } from 'express';
 import * as bcrypt from 'bcrypt';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { Request } from 'express';
-import { adminsRepo, usersRepo } from '../repositories';
+import { adminsRepo, emailVerificationsRepo, usersRepo } from '../repositories';
 import { SessionAdminUser } from '../interfaces';
 import { CreateUserDto } from '../dto';
-import { ConflictException } from '../common/exceptions';
+import { BadRequestException, ConflictException } from '../common/exceptions';
+import { verifyToken } from '../common/app-utils';
 import ErrorMessages from '../enums/error-messages.enum';
 
 export const validateAdminCredentials = async (
@@ -42,7 +44,45 @@ export const registerUser = async (createUserDto: CreateUserDto, res: Response) 
     password: createUserDto.password,
   });
 
-  await usersRepo.save(user);
+  return await usersRepo.save(user);
+};
 
-  return null;
+export const verifyUser = async (token: string) => {
+  try {
+    const { sub } = await verifyToken(token, process.env.EMAIL_VERIFICATION_TOKEN_SECRET);
+
+    const emailVerification = await emailVerificationsRepo.findOne({
+      where: {
+        user: {
+          id: sub,
+        },
+      },
+    });
+
+    if (!emailVerification) {
+      throw new BadRequestException(ErrorMessages.EMAIL_VERIFICATION_EXPIRED);
+    }
+
+    const now = new Date().getTime();
+
+    if (now >= emailVerification.expiresIn.getTime()) {
+      await emailVerificationsRepo.remove(emailVerification);
+      throw new BadRequestException(ErrorMessages.EMAIL_VERIFICATION_EXPIRED);
+    }
+
+    await usersRepo.update({ id: sub }, { emailVerifiedAt: new Date() });
+    await emailVerificationsRepo.remove(emailVerification);
+
+    return 'OK';
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      throw new BadRequestException(ErrorMessages.EMAIL_VERIFICATION_EXPIRED);
+    }
+
+    if (error instanceof JsonWebTokenError) {
+      throw new BadRequestException(ErrorMessages.INVALID_EMAIL_VERIFICATION_LINK);
+    }
+
+    throw error;
+  }
 };
--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -3,11 +3,12 @@ import passport from 'passport';
 import routeHandler from '../middleware/route-handler';
 import schemaValidator from '../middleware/schema-validator';
 import { loginSchema as adminLoginSchema } from '../schemas/admin.schema';
-import { createSchema } from '../schemas/user.schema';
+import { createSchema, verificationSchema } from '../schemas/user.schema';
 import { StatusCode } from '../enums/status-code.enum';
 import { CreateUserDto } from '../dto';
-import { loginAdmin, registerUser } from '../controllers/auth.controller';
+import { loginAdmin, registerUser, verifyUser } from '../controllers/auth.controller';
 import adminAuth from '../middleware/admin-auth';
+import { sendVerificationEmail } from '../controllers/email-verifications.controller';
 
 const router = Router();
 
@@ -33,6 +34,21 @@ router.post(
   schemaValidator(createSchema),
   routeHandler((req: Request, res: Response) => {
     return registerUser(req.body as CreateUserDto, res);
+  }, StatusCode.OK)
+);
+
+router.post(
+  '/verify',
+  schemaValidator(verificationSchema),
+  routeHandler((req: Request, res: Response) => {
+    return sendVerificationEmail(req.body.email);
+  }, StatusCode.OK)
+);
+
+router.get(
+  '/verify/:token',
+  routeHandler((req: Request, res: Response) => {
+    return verifyUser(req.params['token']);
   }, StatusCode.OK)
 );
 
