71300d17ce4f78a8b7983d4aca9f66eb05db78b2
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-12 22:10:22 +0330

    refactor: route handler middleware

--- backend/src/middleware/route-handler.ts
+++ backend/src/middleware/route-handler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 type RequestHandler = (req: Request, res: Response, next: NextFunction) => any;
 
 export default function (handler: RequestHandler) {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     try {
       handler(req, res, next);
     } catch (err) {
