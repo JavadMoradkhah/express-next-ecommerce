6b0dc21d2f89635626b7529cfdd34725f7c12b94
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-12 22:18:10 +0330

    feat: add the login route for regular users

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express';
 import passport from 'passport';
 import * as adminLocalStrategy from '../auth/strategies/admin-local.strategy';
+import * as userLocalStrategy from '../auth/strategies/user-local.strategy';
 import routeHandler from '../middleware/route-handler';
 import schemaValidator from '../middleware/schema-validator';
 import { loginSchema as adminLoginSchema } from '../schemas/admin.schema';
-import { createSchema, verificationSchema } from '../schemas/user.schema';
-import { loginAdmin, registerUser, verifyUser } from '../controllers/auth.controller';
+import { createSchema, loginSchema, verificationSchema } from '../schemas/user.schema';
+import { loginAdmin, loginUser, registerUser, verifyUser } from '../controllers/auth.controller';
 import adminAuth from '../middleware/admin-auth';
 import { sendVerificationEmail } from '../controllers/email-verifications.controller';
 
@@ -16,6 +17,13 @@ router.post(
   schemaValidator(adminLoginSchema),
   passport.authenticate(adminLocalStrategy.name),
   routeHandler(loginAdmin)
+);
+
+router.post(
+  '/login',
+  schemaValidator(loginSchema),
+  passport.authenticate(userLocalStrategy.name),
+  routeHandler(loginUser)
 );
 
 router.get(
