0a53e4dc260f600827f9dd421f8297f7dfbf7592
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-13 10:15:58 +0330

    feat: improve the route handler middleware error handling

--- backend/src/middleware/route-handler.ts
+++ backend/src/middleware/route-handler.ts
@@ -1,13 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
+import { StatusCode } from '../enums/status-code.enum';
 
 type RequestHandler = (req: Request, res: Response, next: NextFunction) => any;
 
-export default function (handler: RequestHandler) {
+type Options = { statusCode?: StatusCode };
+
+export default function (handler: RequestHandler, options?: Options) {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      handler(req, res, next);
+      const result = handler(req, res, next);
+
+      const status = options?.statusCode
+        ? options?.statusCode
+        : req.method.toLowerCase() === 'post'
+        ? StatusCode.CREATED
+        : StatusCode.OK;
+
+      if (result instanceof Promise) {
+        return result.then((value) => res.status(status).send(value)).catch(next);
+      }
+
+      return res.status(status).send(result);
     } catch (err) {
-      next(err);
+      return next(err);
     }
   };
 }
