583cefa4c0efd07989dbc8578705f0d09ec1a931
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-16 13:43:40 +0330

    refactor: rename the role middleware to admin and grant access only for super admin accounts in the middleware

--- backend/src/middleware/admin.ts
+++ backend/src/middleware/admin.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { ForbiddenException } from '../common/exceptions';
-import { Role } from '../entities';
 import ErrorMessages from '../enums/error-messages.enum';
 import { SessionAdminUser } from '../interfaces';
+import { Role } from '../entities';
 
-export default (roles: Role[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+export default () => {
+  return (req: Request, res: Response, next: NextFunction) => {
     const user = req?.user as SessionAdminUser;
 
-    if (!req.isAuthenticated() || !roles.includes(user?.role)) {
+    if (!req.isAuthenticated() || user?.role !== Role.SUPER_ADMIN) {
       return next(new ForbiddenException(ErrorMessages.FORBIDDEN_RESOURCE));
     }
 
