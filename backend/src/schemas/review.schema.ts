fed0236c4b93147f9f49a5bcb2f2785589c0269e
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-16 16:11:45 +0330

    feat: set the review schema labels

--- backend/src/schemas/review.schema.ts
+++ backend/src/schemas/review.schema.ts
@@ -2,12 +2,13 @@ import Joi from 'joi';
 import { CreateReviewDto, UpdateReviewDto } from '../dto';
 
 const schema: Record<keyof CreateReviewDto, Joi.AnySchema> = {
-  product: Joi.string().uuid(),
-  rating: Joi.number().min(0).max(5).precision(1),
-  comment: Joi.string().max(1000),
+  product: Joi.string().label('Product').uuid(),
+  rating: Joi.number().label('Rating').min(0).max(5),
+  comment: Joi.string().label('Comment').max(1000),
 };
 
-export const updateSchema = Joi.object<UpdateReviewDto>(schema).min(1);
+export const updateSchema = Joi.object<UpdateReviewDto>(schema).min(1).label('Review');
+
 
 export const createSchema = Joi.object<CreateReviewDto>({
   product: schema.product.required(),
