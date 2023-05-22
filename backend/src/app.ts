ad196268380395d1bf305c4d324f2fffada6e4b5
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-22 22:30:16 +0330

    refactor: combine the main app.ts module with the app.ts module in the "bootstrap" directory

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,41 @@
 require('dotenv').config();
 import 'reflect-metadata';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import passport from 'passport';
-import { createApp } from './bootstrap/app';
-import { AppDataSource } from './config/database';
-import { getRedisClient } from './config/redis';
 import * as adminLocalStrategy from './auth/strategies/admin-local.strategy';
 import * as userLocalStrategy from './auth/strategies/user-local.strategy';
 import * as adminController from './controllers/admins.controller';
 import * as usersController from './controllers/users.controller';
 import { SessionAdminUser, SessionUser } from './interfaces';
+import { AppDataSource } from './config/database';
+import { getRedisClient } from './config/redis';
 import { AdminRoles } from './entities';
 import { CookieUser } from './interfaces/cookie-user';
+import sessionOptions from './config/session-options';
+import appRouter from './routes/app.router';
+import errorsMiddleware from './middleware/errors';
 
-const app = createApp();
-const port = parseInt(process.env.PORT, 10) ?? 5000;
+const app = express();
 const redisClient = getRedisClient();
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+}
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session(sessionOptions(redisClient)));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(appRouter);
+app.use(errorsMiddleware);
+
+const port = parseInt(process.env.PORT, 10) ?? 5000;
 
 passport.use(adminLocalStrategy.name, adminLocalStrategy.strategy);
 passport.use(userLocalStrategy.name, userLocalStrategy.strategy);
