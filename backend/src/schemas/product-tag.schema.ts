89a5ee3e8199fc42a0f3361dec5a0f040c463cdf
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-28 21:28:29 +0330

    refactor: remove product tag update schema

--- backend/src/schemas/product-tag.schema.ts
+++ backend/src/schemas/product-tag.schema.ts
@@ -1,12 +1,10 @@
 import Joi from 'joi';
-import { CreateProductTagDto, UpdateProductTagDto } from '../dto';
+import { CreateProductTagDto } from '../dto';
 
 const schema: Record<keyof CreateProductTagDto, Joi.AnySchema> = {
   product: Joi.string().uuid(),
   tag: Joi.string().uuid(),
 };
-
-export const updateSchema = Joi.object<UpdateProductTagDto>(schema).min(1);
 
 export const createSchema = Joi.object<CreateProductTagDto>({
   product: schema.product.required(),
