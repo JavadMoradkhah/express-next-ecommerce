651ea10b72382ac6a44cf3b9fd15f481a3b8e055
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-28 17:01:57 +0330

    feat: add regex pattern to slug field in the the brand schema

--- backend/src/schemas/brand.schema.ts
+++ backend/src/schemas/brand.schema.ts
@@ -3,7 +3,11 @@ import { CreateBrandDto, UpdateBrandDto } from '../dto';
 
 const schema: Record<keyof CreateBrandDto, Joi.AnySchema> = {
   name: Joi.string().max(50),
-  slug: Joi.string().max(50),
+  slug: Joi.string()
+    .max(50)
+    .regex(/^[a-z0-9]+(-?[a-z0-9]+)*$/, {
+      name: 'slug',
+    }),
 };
 
 export const updateSchema = Joi.object<UpdateBrandDto>(schema).min(1);
