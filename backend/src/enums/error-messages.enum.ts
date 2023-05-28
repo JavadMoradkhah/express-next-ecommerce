3320aa1d9946660d4833b27262fa08e3c535df6f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-05-28 14:18:27 +0330

    feat: add faq category error messages

--- backend/src/enums/error-messages.enum.ts
+++ backend/src/enums/error-messages.enum.ts
@@ -5,6 +5,9 @@ const enum ErrorMessages {
   CATEGORY_NOT_FOUND = 'The category was not found with the given id',
   PARENT_CATEGORY_NOT_FOUND = 'The parent category was not found with the given id',
   CATEGORY_ALREADY_EXISTS = 'A category already exists with the given slug',
+  // Faq category
+  FAQ_CATEGORY_NOT_FOUND = 'The category was not found with the given id',
+  FAQ_CATEGORY_ALREADY_EXISTS = 'A category already exists with the given name',
   // Color
   COLOR_NOT_FOUND = 'The color was not found with the given id',
   COLOR_ALREADY_EXISTS = 'A color already exists with the given name or code',
