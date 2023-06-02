ba7831ff31e51b46945b73a1e29cbacbb7466b99
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-02 18:03:33 +0330

    refactor: promotion schema

--- backend/src/schemas/promotion.schema.ts
+++ backend/src/schemas/promotion.schema.ts
@@ -2,17 +2,20 @@ import Joi from 'joi';
 import { CreatePromotionDto, UpdatePromotionDto } from '../dto';
 
 const schema: Record<keyof CreatePromotionDto, Joi.AnySchema> = {
-  product: Joi.string().uuid(),
-  startDate: Joi.string().isoDate(),
-  endDate: Joi.string().isoDate(),
-  discountRate: Joi.number().integer().greater(0).less(100),
+  product: Joi.string().uuid().required(),
+  startDate: Joi.string()
+    .regex(/^[0-9]{4}(-[0-9]{2}){2}$/)
+    .required(),
+  endDate: Joi.string()
+    .regex(/^[0-9]{4}(-[0-9]{2}){2}$/)
+    .required(),
+  discountRate: Joi.number().integer().greater(0).less(100).required(),
 };
 
-export const updateSchema = Joi.object<UpdatePromotionDto>(schema).min(1);
-
-export const createSchema = Joi.object<CreatePromotionDto>({
-  product: schema.product.required(),
+export const updateSchema = Joi.object<UpdatePromotionDto>({
   startDate: schema.startDate,
-  endDate: schema.endDate.required(),
-  discountRate: schema.discountRate.required(),
+  endDate: schema.endDate,
+  discountRate: schema.discountRate,
 });
+
+export const createSchema = Joi.object<CreatePromotionDto>(schema);
