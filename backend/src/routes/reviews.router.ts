a1ced3a5e1b6c436dd535801c1ce2e11fa65ae7f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-12 22:06:23 +0330

    refactor: remove the admin routes from reviews router

--- backend/src/routes/reviews.router.ts
+++ backend/src/routes/reviews.router.ts
@@ -1,54 +1,28 @@
 import { Router, Request, Response } from 'express';
-import {
-  create,
-  findAll,
-  findUserReviews,
-  findOne,
-  remove,
-  update,
-  updateStatus,
-} from '../controllers/reviews.controller';
+import { create, findAll, remove, update } from '../controllers/reviews.controller';
 import routeHandler from '../middleware/route-handler';
-import admin from '../middleware/admin';
 import auth from '../middleware/auth';
 import idValidator from '../middleware/id-validator';
 import schemaValidator from '../middleware/schema-validator';
-import { createSchema, updateSchema, updateStatusSchema } from '../schemas/review.schema';
+import { createSchema, updateSchema } from '../schemas/review.schema';
 import { StatusCode } from '../enums/status-code.enum';
-import { CreateReviewDto, UpdateReviewDto, UpdateReviewStatusDto } from '../dto';
+import { CreateReviewDto, UpdateReviewDto } from '../dto';
 import { SessionUser } from '../interfaces';
 
 const router = Router();
 
+router.use(auth());
+
 router.get(
   '/',
-  admin(),
-  routeHandler((req: Request, res: Response) => {
-    return findAll();
-  })
-);
-
-router.get(
-  '/mine',
-  auth(),
   routeHandler((req: Request, res: Response) => {
     const user = req.user as SessionUser;
-    return findUserReviews(user.id);
-  })
-);
-
-router.get(
-  '/:id',
-  admin(),
-  idValidator(),
-  routeHandler((req: Request, res: Response) => {
-    return findOne(req.params.id, { includeRelations: true });
+    return findAll(user.id);
   })
 );
 
 router.post(
   '/',
-  auth(),
   schemaValidator(createSchema),
   routeHandler((req: Request, res: Response) => {
     const user = req.user as SessionUser;
@@ -58,7 +32,6 @@ router.post(
 
 router.patch(
   '/:id',
-  auth(),
   idValidator(),
   schemaValidator(updateSchema),
   routeHandler((req: Request, res: Response) => {
@@ -67,19 +40,8 @@ router.patch(
   })
 );
 
-router.patch(
-  '/:id/status',
-  admin(),
-  idValidator(),
-  schemaValidator(updateStatusSchema),
-  routeHandler((req: Request, res: Response) => {
-    return updateStatus(req.params.id, req.body as UpdateReviewStatusDto);
-  })
-);
-
 router.delete(
   '/:id',
-  auth(),
   idValidator(),
   routeHandler(
     (req: Request, res: Response) => {
