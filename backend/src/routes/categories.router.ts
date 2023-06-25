af70a5bc5a75b9d7c310d50a19d74c73aa84f53e
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-06-25 17:29:53 +0330

    refactor: remove the admin routes from categories router

--- backend/src/routes/categories.router.ts
+++ backend/src/routes/categories.router.ts
@@ -1,12 +1,7 @@
 import { Router, Request, Response } from 'express';
-import { create, findAll, findOne, remove, update } from '../controllers/categories.controller';
+import { findAll, findOne } from '../controllers/categories.controller';
 import routeHandler from '../middleware/route-handler';
 import idValidator from '../middleware/id-validator';
-import schemaValidator from '../middleware/schema-validator';
-import { createSchema, updateSchema } from '../schemas/category.schema';
-import adminAuth from '../middleware/auth';
-import { CreateCategoryDto } from '../dto';
-import { StatusCode } from '../enums/status-code.enum';
 
 const router = Router();
 
@@ -23,37 +18,6 @@ router.get(
   routeHandler((req: Request, res: Response) => {
     return findOne(req.params.id);
   })
-);
-
-router.post(
-  '/',
-  adminAuth(),
-  schemaValidator(createSchema),
-  routeHandler((req: Request, res: Response) => {
-    return create(req.body as CreateCategoryDto);
-  })
-);
-
-router.patch(
-  '/:id',
-  adminAuth(),
-  idValidator(),
-  schemaValidator(updateSchema),
-  routeHandler((req: Request, res: Response) => {
-    return update(req.params.id, req.body as CreateCategoryDto);
-  })
-);
-
-router.delete(
-  '/:id',
-  adminAuth(),
-  idValidator(),
-  routeHandler(
-    (req: Request, res: Response) => {
-      return remove(req.params.id);
-    },
-    { statusCode: StatusCode.NO_CONTENT }
-  )
 );
 
 export default router;
